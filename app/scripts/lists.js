7375d6ae1157085776964bbd08cf742f3d9aaf14
brookebc <brooke.casey@gmail.com>
2014-04-30 06:35:55 -0400

    getting things started (layout, list items and files)

---
+++ app/scripts/lists.js
@@ -0,0 +1,10 @@
+//this is where i will put data from some text 
+//that is a placeholder and test on the app
+
+var todoitem = [
+{
+	activity: "Make this super todo app"
+},{
+	activity: "Go to the beach"
+},
+];
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,5 @@
 console.log('\'Allo \'Allo!');
+
+var newToDo = _.template($("#todoitemTmpl").html(), list);
+
+  $("list").append(newToDo);
