8c6e6902b928eccf9f69fa807fe46484b6906f5c
brookebc <brooke.casey@gmail.com>
2014-04-30 07:19:22 -0400

    added lines on app and form in module

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,27 @@ console.log('\'Allo \'Allo!');
 
 var newToDo = _.template($("#todoitemTmpl").html(), list);
 
-  $("list").append(newToDo);
+	$("list").append(newToDo);
+
+$("#newToDoForm").on("add", function(e) {
+  		e.preventDefault();
+  		// var formData = $(this).serializeArray();
+  		// console.log($(this).serializeArray());
+  		var postTitle = $(".newPostTitle").val();
+  		console.log(postContentForm);
+
+  		var newPostObj = {
+  					title: postTitle,
+  					date: "Tuesday, 29, 2014",
+  					content: postContentForm,
+  					author: authorPostForm
+  		};
+  		posts.unshift(newPostObj);
+  		var myPostsString = _.template($("#todoTmpl").html(), list);
+
+  		$(".newPostTitle").val("");
+  		$("#myModal").modal("hide");
+  		$("section").html(myPostsString);
+
+
+  });
